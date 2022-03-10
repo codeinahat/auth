a67a0c636296d8776c14241cc6e10fdb01093003
Richard Chapman Gomez <chapman.gomez.richard@gamil.com>
2022-03-10 00:14:44 -0500

    ADDED TYPES

--- src/core/model/hashing.ts
+++ src/core/model/hashing.ts
@@ -1,15 +1,18 @@
 import { randomBytes, createHash } from 'crypto';
 import { HashedPasswordResult } from '../interfaces/hased-password.interface';
+import { algos } from '../types/algos.type';
+
 
 
 /**
  * Creates a hashed version of the password using the sha256 
  * algorithm unless a different one is provided, and a salt created from random bytes.
  * @param password - Password to be hashed
- * @param algo - You can specify hashing algorihtm if desired. Defaults to SHA256 is not algorithm provided. 
+ * @param algo - You can specify hashing algorithm if desired. Defaults to SHA256 is not algorithm provided.
+
  * @returns {HashedPasswordResult} hashed_password and salt
  */
-export function hashPassword (password: string, algo?: string):  HashedPasswordResult {
+export function hashPassword (password: string, algo?: algos):  HashedPasswordResult {
     
     // creates salt
     const salt = randomBytes(26).toString('hex');
